4d1a7a56962d36860bffd4f9de05a9b686d7f8cf
Jeong Kim <joshkim83@outlook.com>
2017-08-30 20:44:48 -0600

    added rating control class

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,15 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Jeong Kim on 8/30/17.
+//  Copyright © 2017 Jeong Kim. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+
+
+}
