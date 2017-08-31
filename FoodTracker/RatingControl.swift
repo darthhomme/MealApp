d7ea3abe7950582b639efe8100e3ea49e6f21962
Jeong Kim <joshkim83@outlook.com>
2017-08-30 21:42:33 -0600

    added rating capability

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -13,8 +13,12 @@ import UIKit
     //MARK: Properties
     private var ratingButtons = [UIButton]()
     
-    var rating = 0
-    
+    var rating = 0 {
+        didSet {
+            updateButtonSelectionStates()
+        }
+
+    }
     @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0){
         didSet{
             setupButtons()
@@ -40,14 +44,27 @@ import UIKit
     //MARK: Button Action
     
     func ratingButtonTapped(button: UIButton) {
-        print("Button pressed 👍")
+        guard let index = ratingButtons.index(of: button) else {
+            fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
+        }
+        
+        // Calculate the rating of the selected button
+        let selectedRating = index + 1
+        
+        if selectedRating == rating {
+            // If the selected star represents the current rating, reset the rating to 0.
+            rating = 0
+        } else {
+            // Otherwise set the rating to the selected star
+            rating = selectedRating
+        }
     }
     
     //MARK: Private Methods
     
     private func setupButtons() {
         
-        // clear any existing buttons
+        // Clear any existing buttons
         for button in ratingButtons {
             removeArrangedSubview(button)
             button.removeFromSuperview()
@@ -61,9 +78,9 @@ import UIKit
         let highlightedStar = UIImage(named:"highlightedStar", in: bundle, compatibleWith: self.traitCollection)
         
         for _ in 0..<starCount {
-            
             // Create the button
             let button = UIButton()
+            
             // Set the button images
             button.setImage(emptyStar, for: .normal)
             button.setImage(filledStar, for: .selected)
@@ -81,10 +98,19 @@ import UIKit
             // Add the button to the stack
             addArrangedSubview(button)
             
-            
             // Add the new button to the rating button array
             ratingButtons.append(button)
         }
         
+        updateButtonSelectionStates()
+        
+    }
+    
+    private func updateButtonSelectionStates() {
+        
+        for (index, button) in ratingButtons.enumerated() {
+            // If the index of a button is less than the rating, that button should be selected.
+            button.isSelected = index < rating
+        }
     }
 }
